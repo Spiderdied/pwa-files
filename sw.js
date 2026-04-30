5b2a2f58553d8b7b1ac7633240ad2a6379e9764e
Ubad34 <153395328+Spiderdied@users.noreply.github.com>
2026-04-30 18:33:27 +0300

    Refactor service worker for caching and fetching

--- sw.js
+++ sw.js
@@ -1,17 +1,26 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("blog-cache").then(cache => {
-      return cache.addAll([
-        "https://ubad34.blogspot.com/"
-      ]);
+// اسم الكاش
+const CACHE_NAME = "ubad-app-v1";
+
+// الملفات اللي تتحفظ
+const urlsToCache = [
+  "/",
+  "https://ubad34.blogspot.com/"
+];
+
+// تثبيت
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// تشغيل
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
